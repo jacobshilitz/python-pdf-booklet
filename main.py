912156bc947f6c544bd7b2194a8be9ba1a2e0549
Jacob Shilitz <jacobshilitz@gmail.com>
2024-05-09 15:25:58 -0400

    Refactor 'get_basename_file' function using pathlib
    The 'get_basename_file' function in 'main.py' has been refactored to use the pathlib library instead of os.path for more readable and cleaner code. The output file string assembly has also been updated to be compatible with the new pathlib object type.

--- main.py
+++ main.py
@@ -22,13 +22,12 @@ else:
     sys.exit(1)
 
 def get_basename_file(file_path):
-    base_name = os.path.basename(file_path)  # Returns 'filename.extension'
-    file_name_without_extension = os.path.splitext(base_name)[0]  # Returns 'filename'
-    return file_name_without_extension
+    path = Path(file_path)
+    return path.parent / path.stem
 
 def booklet_pdf(data):
     pdf_file = data['input_pdf']
-    output_file = data['output_pdf'] if data['output_pdf'] else get_basename_file(pdf_file) + "_booklet.pdf"
+    output_file = data['output_pdf'] if data['output_pdf'] else str(get_basename_file(pdf_file)) + "_booklet.pdf"
     hp = data.get('hp', False)
     resize_only = data.get('resize_only', False)
     ltr = data.get('ltr', False)
