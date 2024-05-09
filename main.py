2f3f84236ee0ad9657772778dd22b390e63e95bb
Jacob Shilitz <jacobshilitz@gmail.com>
2024-05-09 14:32:21 -0400

    Refine scaling in PDF booklet function and add new paper sizes
    The code refines the PDF booklet function by improving page scaling, specifically accommodating for crop boxes. It also adds flexibility by allowing users to specify paper size, now including support for 'LTR' and '11x17'. Overall, these changes lead to improved accuracy and flexibility in resizing and scaling PDFs.

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from pathlib import Path
 import argparse
 import pycpdflib
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 __package_name__ = 'booklet'
 
+INCHES_PER_POINT = 72
+PAPER_SIZES = {
+    'LTR': {'w': 11, 'h': 8.5},
+    '11x17': {'w': 11, 'h': 17},
+}
 
 if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
     lib_path = Path(getattr(sys, '_MEIPASS', Path.cwd()))
@@ -17,25 +22,71 @@ else:
     sys.exit(1)
 
 
-def booklet_pdf(args):
-    pdf_file = args.input_pdf
-    output_file = args.output_pdf if args.output_pdf else "booklet.pdf"
+def booklet_pdf(data):
+    pdf_file = data['input_pdf']
+    output_file = data['output_pdf'] if data['output_pdf'] else "booklet.pdf"
+    hp = data.get('hp', False)
+    resize_only = data.get('resize_only', False)
+    ltr = data.get('ltr', False)
+    paper = data.get('paper', 'LTR')
 
     source_pdf = pycpdflib.fromFile(pdf_file, '')
     pdf_range = pycpdflib.all(source_pdf)
-    pdf_size = pycpdflib.getMediaBox(source_pdf, 1)
-    total_pages = pycpdflib.pages(source_pdf)
 
+    pdf_size = pycpdflib.getMediaBox(source_pdf, 1)
     pdf_height = pdf_size[3]
     pdf_width = pdf_size[1]
+    total_pages = pycpdflib.pages(source_pdf)
 
-    letter_x_size = 5.5 * 72
-    letter_y_size = 8.5 * 72
+    # if it has crop we use the cropsize
+    if pycpdflib.hasBox(source_pdf, 1, '/CropBox'):
+        pdf_crop_size = pycpdflib.getCropBox(source_pdf, 1)
 
-    x_scale = letter_y_size / pdf_height
-    y_scale = letter_x_size / pdf_width
+        pdf_crop_width = pdf_crop_size[1] - pdf_crop_size[0]
+        pdf_crop_height = pdf_crop_size[3] - pdf_crop_size[2]
 
-    pycpdflib.scalePages(source_pdf, pdf_range, y_scale, x_scale)
+        print("Original PDF crop size w:", round(pdf_crop_width / INCHES_PER_POINT, 2), "h:",
+              round(pdf_crop_height / INCHES_PER_POINT, 2))
+
+        pdf_height = pdf_crop_height
+        pdf_width = pdf_crop_width
+
+    #     remove cropp age
+    #     pycpdflib.getCropBox()
+
+    print('Original PDF size w:', round(pdf_width / INCHES_PER_POINT, 2), 'h:', round(pdf_height / INCHES_PER_POINT, 2))
+
+    paper_size = PAPER_SIZES.get(paper, False)
+
+    if not paper_size:
+        print("Invalid paper size")
+        sys.exit(1)
+
+    desired_width_in_inch = paper_size.get('w')
+    desired_height_in_inch = paper_size.get('h')
+
+    if not resize_only:
+        #      rotate page size and split width in half
+        if paper_size.get('h') > paper_size.get('w'):
+            desired_width_in_inch = paper_size.get('h')
+            desired_height_in_inch = paper_size.get('w')
+
+        desired_width_in_inch = desired_width_in_inch / 2
+    print("_______________")
+    print('Desired width per page: ' + str(desired_width_in_inch))
+    print('Desired height per page: ' + str(desired_height_in_inch))
+    print("_______________")
+
+    desired_width = desired_width_in_inch * INCHES_PER_POINT
+    desired_height = desired_height_in_inch * INCHES_PER_POINT
+
+    print("")
+
+    height_scale = desired_height / pdf_height
+    width_scale = desired_width / pdf_width
+
+    # apply the scale
+    pycpdflib.scalePages(source_pdf, pdf_range, width_scale, height_scale)
 
     remainder = total_pages % 4
     blank_pages = 0 if remainder == 0 else 4 - remainder
@@ -52,18 +103,19 @@ def booklet_pdf(args):
         page_order.append(pages.pop(0))
         page_order.append(pages.pop())
 
-    if args.hp:
+    if hp:
         print("hp mode is running")
         page_order = hp_sort(page_order)
 
-    if not args.resize_only:
+    if not resize_only:
         source_pdf = pycpdflib.selectPages(source_pdf, page_order)
 
-        pycpdflib.impose(source_pdf, 2, 1, False, False, (not args.ltr), False, False, False, False, False)
+        pycpdflib.impose(source_pdf, 2, 1, False, False, (not ltr), False, False, False, False, False)
 
     pycpdflib.toFile(source_pdf, output_file, False, False)
 
     print("Resized PDF to", output_file)
+
 
 def hp_sort(arr):
     # splitting list into sub-lists of 4 elements
@@ -79,12 +131,17 @@ def hp_sort(arr):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog=__package_name__ ,description='Convert pdf to booklet')
+    parser = argparse.ArgumentParser(prog=__package_name__, description='Convert pdf to booklet')
     parser.add_argument('input_pdf', type=str, help='Path to the input pdf file')
     parser.add_argument('output_pdf', type=str, help='Path to the output pdf file', nargs='?')
     parser.add_argument('--hp', help='HP mode', action='store_true')
     parser.add_argument('--ltr', help='left to right', action='store_true')
     parser.add_argument('--resize_only', help='just resize, no booklet', action='store_true')
 
+    parser.add_argument('--paper', type=str, help='Specify paper size. Available options: LTR, 11x17',
+                        choices=['LTR', '11x17'], default='LTR')
+
     args = parser.parse_args()
-    booklet_pdf(args)
+    data = vars(args)
+
+    booklet_pdf(data)
