93587050e27a2e019debcfcf637fdc1f32d0e917
Jacob Shilitz <jacobshilitz@gmail.com>
2024-06-11 00:04:03 -0400

    Update PyInstaller version and disable package auto-update
    Updated the PyInstaller version in the requirements.txt file from 6.6.0 to 6.8.0. In build.py, the auto-update feature for packages has been commented out to prevent unintended version changes.

--- build.py
+++ build.py
@@ -7,10 +7,10 @@ import subprocess as sbp
 
 if __name__ == '__main__':
     #update pakcages
-    pkgs = eval(str(sbp.run("pip list -o --format=json", shell=True,
-                            stdout=sbp.PIPE).stdout, encoding='utf-8'))
-    for pkg in pkgs:
-        sbp.run("pip install --upgrade " + pkg['name'], shell=True)
+    # pkgs = eval(str(sbp.run("pip list -o --format=json", shell=True,
+    #                         stdout=sbp.PIPE).stdout, encoding='utf-8'))
+    # for pkg in pkgs:
+    #     sbp.run("pip install --upgrade " + pkg['name'], shell=True)
 
 
     try:
